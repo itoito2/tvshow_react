d761b9ce280fd7e592b6e4bb4c4ec91cc9f95886
itoito2 <105725358+itoito2@users.noreply.github.com>
2024-10-23 16:12:02 +0200

    Update Docs.js

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -6,6 +6,8 @@ function Docs() {
             <h1>Welcome to docs</h1>
             <h3>Here you will learn how on to use this website.</h3>
 
+            <h5><a href="https://www.tvmaze.com/api">https://www.tvmaze.com/api</a></h5>
+
             <h4>Now that everythings clear, <a href="/browse">proceed to the website.</a></h4>
         </div>
     )

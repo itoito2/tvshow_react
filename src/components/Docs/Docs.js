a2cc0a4099c7fffc4f00b3c4341f70d9cffab855
itoito2 <105725358+itoito2@users.noreply.github.com>
2024-10-24 08:16:29 +0200

    Update Docs.js

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -6,7 +6,7 @@ function Docs() {
             <h1>Welcome to docs</h1>
             <h3>Here you will learn how on to use this website.</h3>
 
-            <h5><a href="https://www.tvmaze.com/api">https://www.tvmaze.com/api</a></h5>
+            <h5>Please view <a href="file:///.\documentation.pdf" rel="noreferrer" target="_blank" className="docs">the documentation</a> before proceeding.</h5>
 
             <h4>Now that everythings clear, <a href="/browse">proceed to the website.</a></h4>
         </div>

ffbed5f2044c5362ffe4fb1cccf287b1d8abea43
itoito2 <105725358+itoito2@users.noreply.github.com>
2024-10-24 06:36:04 +0200

    add filter

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,7 +5,8 @@ import Loading from '../Loading/Loading';
 
 function SearchBar() {
     const [search_term, set_search_term] = useState("")
-    const [search_res, set_search_res] = useState([]);
+    const [search_res, set_search_res] = useState([])
+    const [filter, set_filter] = useState("")
     const [is_loading, set_is_loading] = useState(false)
 
     const fetch_data = async() => {
@@ -18,21 +19,51 @@ function SearchBar() {
                 }
                 const result = await response.json()
                 set_search_res(result)
-                set_is_loading(false)
             } catch(e) {
                 console.log("Error occurred:", e)
             }
         }
+        set_is_loading(false)
+    }
+
+    const apply_filter = () => {
+        let filtered_shows = [...search_res]
+
+        switch (filter) {
+            case "average_rating":
+                filtered_shows = filtered_shows.sort((a, b) => {
+                    const ratingA = a.show.rating?.average || 0;
+                    const ratingB = b.show.rating?.average || 0;
+                    return ratingB - ratingA;  // Sort in decreasing order
+                });
+                break
+
+            case "ongoing":
+                filtered_shows = filtered_shows.filter(tvshow => tvshow.show?.status === "Running")
+                break
+
+            default:
+                break
+        }
+
+        return filtered_shows
     }
 
     return(
         <div>
             <h4>Search for a TV Show: </h4>
             <input type="text" value={search_term} onChange={e => set_search_term(e.target.value)} name="search_term"/> <br/>
+            Filter by: <select onChange={e => set_filter(e.target.value)}>
+                <option value="" selected>None</option>
+                <option value="average_rating">Average Rating</option>
+                <option value="ongoing" >Ongoing</option>
+            </select><br/>
             <button onClick={() => fetch_data()}>Search</button>
             {is_loading && <Loading />}
             {
-                search_res.length > 0 ? <TVShowList shows={search_res} />  : ""
+                search_res.length > 0 ? (
+                    filter.length > 0 ? <TVShowList shows={apply_filter()}/> : <TVShowList shows={search_res} />
+                ) : ""
             }
         </div>
     )
